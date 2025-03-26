7677f97dd050b4ee9e74885c58adf3bf795f96b7
Sachin <sachin238@gmail.com>
2025-03-26 14:25:04 +0530

    adding rspec for various combinations of comma separted numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns the number itself if only one number is provided" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it "returns the sum of two numbers" do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+    it "returns the sum of multiple numbers" do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
   end
 end
